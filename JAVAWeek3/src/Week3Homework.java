65885e27b42454ef04f6a16d9320b41013f5764c
Desmond Young <Desmond.Young25@yahoo.com>
2020-04-11 18:08:00 -0700

    PromineoTech HomeWork

---
+++ JAVAWeek3/src/Week3Homework.java
@@ -0,0 +1,17 @@
+
+public class Week3Homework {
+
+	public static void main(String[] args) {
+		
+		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93, 500};
+		
+		int averageOfAges = ages.length;
+		int firstNumber = ages [ages.length - ages.length];
+		int lastNumber = ages [ages.length - 1];
+		
+		System.out.println("500 - 3 = " + (lastNumber - firstNumber));
+		
+		
+	}
+
+}
