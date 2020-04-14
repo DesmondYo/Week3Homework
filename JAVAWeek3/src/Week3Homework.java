7cdb4b4a9aa4e004a25e5df3d7d66e6cac4f35a3
Desmond Young <Desmond.Young25@yahoo.com>
2020-04-14 07:07:30 -0700

    Updated homework.

--- JAVAWeek3/src/Week3Homework.java
+++ JAVAWeek3/src/Week3Homework.java
@@ -2,16 +2,70 @@
 public class Week3Homework {
 
 	public static void main(String[] args) {
-		
-		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93, 500};
+		//1A & B
+		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93, 21};
 		
 		int averageOfAges = ages.length;
 		int firstNumber = ages [ages.length - ages.length];
 		int lastNumber = ages [ages.length - 1];
 		
-		System.out.println("500 - 3 = " + (lastNumber - firstNumber));
+		System.out.println("21 - 3 = " + (lastNumber - firstNumber));
 		
+		//1C.
+			int total = 0;
+			
+			for (int i = 0; i < ages.length; i++) {
+				total += ages[i];
+			}
+			int average = total / ages.length;
+			System.out.println("The average is: " + average);
+			
+		//2. 
+			String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+			
+		//2A. 
+			int total2 = 0;
+			
+			for (int i = 0; i < names.length; i++) {
+				total2 += names[i].length();
+			}
+			double averageNumOfLetters = total2 / names.length;
+			System.out.println("The average number of letters are: " + averageNumOfLetters );
+			
+		//2B. 
+			int total3 = 0;
+			
+			String allNames = "";
+			
+			for (int i = 0; i < names.length; i++) {
+				allNames += names[i] + " ";
+			}
+			System.out.println("Here are the all names: " + allNames);
 		
+		//3. How do you access the last element of any array: ages[ages.length - 1];
+		
+		//4. How do you access the first element of any array: ages[0];
+			
+		//5. 
+			int[] nameLengths = new int[names.length];
+			for (int i = 0; i < names.length; i++) {
+				nameLengths[i] = names[i].length();
+			}
+		//6.
+			int sum = 0;
+			for (int i = 0; i < nameLengths.length; i++) {
+				sum += nameLengths[i];
+			}
+			System.out.println(sum);
+			
+		}
 	}
 
-}
+
+
+
+
+
+
+
+
